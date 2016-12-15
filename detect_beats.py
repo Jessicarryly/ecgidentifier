fd56c776ea2c7cb3783bf3e5fab2ef71c90ba387
omegamit6zeichen <markus.adamek@gmail.com>
2016-12-15 15:38:08 +0100

    added SVM

--- detect_beats.py
+++ detect_beats.py
@@ -1,23 +1,20 @@
-
+import matplotlib.pyplot as plot 
 import numpy as np
 import scipy
-import peakutils
-import matplotlib.pyplot as plot 
 from scipy  import signal as sci
 import math
 
 
-def FindEcgPeriods(ecg_band,sample_rate):
-    
+def FindEcgPeriods(ecg_band,sample_rate,ecg_window=1.0):
+   
+    rawecg=ecg_band
+    ecg_window=ecg_window*sample_rate
     w_c=sample_rate*2*math.pi
-    w_l=(30*2*math.pi)/w_c #1Hz
-    w_h=(600*2*math.pi)/w_c #800Hz
-    b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba') #remove drift
-    original_ecg=sci.filtfilt(b, a, ecg_band)
+
     
    
-    w_l=(10*2*math.pi)/w_c #10Hz
-    w_h=(20*2*math.pi)/w_c #30Hz
+    w_l=(20*2*math.pi)/w_c #10Hz
+    w_h=(50*2*math.pi)/w_c #30Hz
     b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba')
     ecg_band=sci.filtfilt(b, a, ecg_band)
    
@@ -27,43 +24,90 @@ def FindEcgPeriods(ecg_band,sample_rate):
 
     ecg_diffed=scipy.diff(ecg_band)
     ecg_diffed=ecg_diffed/max(ecg_diffed)
-    ecg_diffed = np.clip(ecg_diffed,0.3,1)
+    ecg_diffed = np.clip(ecg_diffed,0.5,1)
+    
+    
     #peaks= peakutils.indexes(ecg_diffed)
     peaks=FindTurningPoints(ecg_diffed)
+    
     r_peaks=Find_peak_inRange(ecg_band, peaks)
+    r_peaks_refined=[]
+    prev=0
+    for r in r_peaks:
+        if((r-prev) < ecg_window*0.1):
+            continue
+            
+        r_peaks_refined.append(r)    
+        prev=r;
+        
+        
+    r_peaks = r_peaks_refined
 
+   
+    
+   
+    
     
     ecgs=[]
   
     for i in range(len(r_peaks)-1):
         curr_distance=r_peaks[i+1]-r_peaks[i]
+        if(curr_distance > ecg_window):
+            curr_distance=ecg_window
+       
         endpos=peaks[i]+curr_distance/2
         start_pos=peaks[i]-curr_distance/2
-        if(endpos > len(original_ecg)):
-            endpos=len(original_ecg)
-
-        ecgs.append(EcgPeriod(original_ecg[start_pos:endpos],sample_rate))
-
+        
+        
+        
+     
+        if(endpos > len(rawecg)):
+            endpos=len(rawecg)
+            
+        if(start_pos < 0):
+            start_pos=0
+            
+       
+        try:
+            ecgs.append(EcgPeriod(rawecg[start_pos:endpos],sample_rate))
+        except:
+            print'Could not create ecg'
 
     return ecgs
 
 
-def Find_peak_inRange(signal,peakrange):
+def Find_peak_inRange(signal,peakrange,window=500):
     peaks=[]
-    
-    for i in range(0,len(peakrange)-1,2):
-        sliced = signal[peakrange[i]:peakrange[i+1]]
-        peaks.append(FindTurningPoints(sliced)[0]+peakrange[i])
+ 
+    for i in peakrange:
+        
+        sliceend=i+window
+        
+
+
+        if(sliceend > (len(signal)-1)):
+            sliceend=len(signal)-1
+            
+        if(sliceend-i < window/2):
+            continue
+       
+        sliced = signal[i:sliceend]
+
+        peaks.append(FindTurningPoints(sliced)[0]+i)
+        
     return peaks
 
 
 def FindTurningPoints(signal):
     tps=[]
+    
     for i in range(0,len(signal)-2):
         if((signal[i]-signal[i+1] < 0) and (signal[i+1]-signal[i+2] > 0)):
             tps.append(i+1)
-        
+      
+    
     return tps
+    
 
 
 class EcgPeriod:
@@ -82,27 +126,43 @@ class EcgPeriod:
         self.T_End =0
         self.SampleRate = sample_rate
         self.Signal=ecg_single_rythm
+        self.__Original=ecg_single_rythm
+
         self.__findEcgData()
     
     def __findEcgData(self):
+ 
         w_c=self.SampleRate*2*math.pi
-        w_l=(10*2*math.pi)/w_c #10Hz
-        w_h=(20*2*math.pi)/w_c #30Hz
+        w_l=(20*2*math.pi)/w_c #10Hz
+        w_h=(50*2*math.pi)/w_c #30Hz
         b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba')
         ecg_band=sci.filtfilt(b, a, self.Signal)
         ecg_diffed=scipy.diff(ecg_band)
         ecg_diffed=ecg_diffed/max(ecg_diffed)
-        ecg_diffed = np.clip(ecg_diffed,0.3,1)
-        
+        ecg_diffed = np.clip(ecg_diffed,0.5,1)
         peaks=FindTurningPoints(ecg_diffed)
         self.R_Peak=Find_peak_inRange(ecg_band, peaks)[0]
+
+
         
         
+    def ReMapEcgData(self,target_r_peak_position,targetDataPoints):
+        offset=self.R_Peak-target_r_peak_position
         
+             
+        #print('length',len(sliced),'offset',offset)
+        self.Signal= sci.resample(self.__Original, targetDataPoints)
+       # print(len(self.Signal))
         
-        
-    def MapEcgData(self,targetDataPoints):
-        self.R_Peak = 0
-        
+        self.__findEcgData()
+        offset=self.R_Peak-target_r_peak_position
+        self.R_Peak=target_r_peak_position
+        self.Signal = np.roll(self.Signal,-offset)
+          
+       # plot.plot(self.Signal)
+        #plot.scatter(self.R_Peak,self.Signal[self.R_Peak])
+        #plot.show()
+       # print(len(self.Signal))
+
         
         
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import wfdb
-import numpy as np
-import scipy
-import scipy.ndimage
+
 import matplotlib.pyplot as plot 
 from scipy  import signal as sci
 import math
 import detect_beats as detector
-from numpy import median
-import biosppy.signals as ecgdetector
+import tensorflow as tf
+import random
+from sklearn import svm
+import numpy as np
+import os
 
 sample_rate=1000;
 w_c=sample_rate*2*math.pi
@@ -15,28 +16,86 @@ w_c=sample_rate*2*math.pi
 w_l=(30*2*math.pi)/w_c #1Hz
 w_h=(600*2*math.pi)/w_c #800Hz
 
-
-basepath='//home//markus//Documents//Python//wfdb_data//physiobank//database//ptbdb//'
-sig, fields = wfdb.rdsamp(basepath+'patient001//s0010_re')
-
-
-
-nrow,ncol= sig.shape
-
-b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba')
+ecg_length=600
+evidence_vec =10
+seed =1
+percentage_used_for_training=0.5
+current_id =0
+training_set = []
+test_set = []
 
 
-ecgs=detector.FindEcgPeriods(sig[:,0],sample_rate) 
+basepath='..//wfdb_data//physiobank//database//ptbdb//'
+basepath=os.path.realpath(basepath)
+with open(basepath+'//CONTROLS') as f:
 
-r_peak_after=200
-for ecg in ecgs:
-    offset=ecg.R_Peak-r_peak_after
-    plot.plot(ecg.Signal[offset:])
-   
+    for file in f:
+        file=file.strip('\n')
+        if(file =='patient180/s0545_re'): #ignore this sample
+            continue
+        
+        sig, fields = wfdb.rdsamp(basepath+'//'+file)
+        
+        print(file)
+        
+        nrow,ncol= sig.shape
+        
+        b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba')
+        
+        
+        ecgs=detector.FindEcgPeriods(sig[:,0],sample_rate) 
+        
+        r_peak_after=300
+        w_l=(30*2*math.pi)/w_c #1Hz
+        w_h=(600*2*math.pi)/w_c #800Hz
+        b,a =sci.butter(1, [w_l, w_h], 'bandpass', False, 'ba') #remove drift
+        
+        
+        
+        indices=random.sample(range(0,len(ecgs)),len(ecgs))
+        maximum = int(percentage_used_for_training*len(ecgs))
+
+        for ecg in range(0,maximum):
+            ecgs[ecg].ReMapEcgData(r_peak_after,ecg_length)
+            #print("length", len(data))
+            normalized=sci.filtfilt(b, a, ecgs[ecg].Signal)
+            tuple=current_id, normalized
+            
+            training_set.append(tuple)
+            
+        for ecg in range(maximum,len(ecgs)):
+            ecgs[ecg].ReMapEcgData(r_peak_after,ecg_length)
+            #print("length", len(data))
+            normalized=sci.filtfilt(b, a, ecgs[ecg].Signal)
+            
+            #test_set.append(normalized)
+            
+            tuple=current_id, normalized
+            test_set.append(tuple)
+            
+         
+
     
-plot.show()
+       
+      
+        seed+=1
+        current_id+=1   
 
+gammas=np.logspace(0.001,10,10,base=10)
+accuracy = []
+for gamma_val in gammas:
+    print('current gamma',gamma_val)
+    classifier = svm.SVC(kernel='poly',gamma=gamma_val)
+    
+     
+      
+    classifier.fit([item[1] for item in training_set],
+                   [item[0] for item in training_set])
+    
+    predicted=classifier.predict([item[1] for item in test_set])
+    print(predicted)
+                                        
+    accuracy.append(np.mean([item[0] for item in test_set] == predicted))
 
-
-
-
+plot.plot(gammas,accuracy)
+plot.show
